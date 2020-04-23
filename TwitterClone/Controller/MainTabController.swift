501fb963ab9e92dd7472712fbf336f3f83fe8dea
Muhammat Fandi Mayuso <mayuso_so@outlook.com>
2020-04-23 21:22:11 +0700

    Configure project

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by Muhammat Fandi Mayuso on 23/4/20.
+//  Copyright © 2020 Muhammat Fandi Mayuso. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
