dcd6e3957368ab91ac0efaca4ce234768247dc5e
Muhammat Fandi Mayuso <mayuso_so@outlook.com>
2020-05-07 11:07:13 +0700

    Populate each cell with tweet

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -21,6 +21,12 @@ class FeedController: UICollectionViewController {
         }
     }
     
+    private var tweets = [Tweet]() {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -35,7 +41,7 @@ class FeedController: UICollectionViewController {
     func fetchTweets() {
         
         TweetService.shared.fetchTweets { tweets in
-            print("DEBUG: Tweets are \(tweets)")
+            self.tweets = tweets
         }
     }
     
@@ -77,12 +83,13 @@ extension FeedController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 5
+        return tweets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
+        cell.tweet = tweets[indexPath.row]
         
         return cell
     }
--- TwitterClone/View/TweetCell.swift
+++ TwitterClone/View/TweetCell.swift
@@ -12,6 +12,12 @@ class TweetCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    var tweet: Tweet? {
+        didSet {
+            configure()
+        }
+    }
+    
     private let profileImageView: UIImageView = {
         
         let imageView = UIImageView()
@@ -29,7 +35,6 @@ class TweetCell: UICollectionViewCell {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 14)
         label.numberOfLines = 0
-        label.text = "Some test caption"
         
         return label
     }()
@@ -139,4 +144,11 @@ class TweetCell: UICollectionViewCell {
     }
     
     // MARK: - Helpers
+    
+    func configure() {
+        
+        guard let tweet = tweet else { return }
+        
+        captionLabel.text = tweet.caption
+    }
 }
