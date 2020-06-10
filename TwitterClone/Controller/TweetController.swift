e77b4e1c363c07784db2c8066a4a31e45850f35e
Muhammat Fandi Mayuso <mayuso_so@outlook.com>
2020-06-10 22:14:42 +0700

    Finish up with tap handler

--- TwitterClone/Controller/TweetController.swift
+++ TwitterClone/Controller/TweetController.swift
@@ -129,6 +129,19 @@ extension TweetController: TweetHeaderDelegate {
 extension TweetController: ActionSheetLauncherDelegate {
     
     func didSelect(option: ActionSheetOptions) {
-        print("DEBUG: Optionin controller is \(option.description)")
+        switch option {
+        case .follow(let user):
+            UserService.shared.followUser(uid: user.uid) { (error, ref) in
+                print("DEBUG: Did follow user \(user.username)")
+            }
+        case .unfollow(let user):
+            UserService.shared.unfollowUser(uid: user.uid) { (error, ref) in
+                print("DEBUG: Did unfollow user \(user.username)")
+            }
+        case .report:
+            print("DEBUG: Report Tweet")
+        case .delete:
+            print("DEBUG: Delete Tweet")
+        }
     }
 }
